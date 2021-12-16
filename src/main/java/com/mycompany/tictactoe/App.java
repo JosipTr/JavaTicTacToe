4e833d998a6b5bcf8a6ae11d20cdde93d83de11c
JosipTr <josiptroha@gmail.com>
2021-12-16 16:41:31 +0100

    trying to make simpler code by adding class and implementing methods

--- src/main/java/com/mycompany/tictactoe/App.java
+++ src/main/java/com/mycompany/tictactoe/App.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import my.company.domain.ButtonState;
 
 
 /**
@@ -20,6 +21,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        
+        ButtonState buttonState = new ButtonState();
+        
         
         //Application title
         Parameters params = getParameters();
@@ -44,37 +48,23 @@ public class App extends Application {
         //Label which say whose turn it is
         Label textTurn = new Label("Turn: X");
         textTurn.setFont(Font.font("Monospaced", 40));
-        
-        ArrayList<Button> btnList = createButtonList();
+
         
         //adding buttons to TicTacToe field
+        buttonState.addToLayout(field);
         
-        
-        field.add(btnList.get(0), 0, 0);
-        field.add(btnList.get(1), 1, 0);
-        field.add(btnList.get(2), 2, 0);
-        field.add(btnList.get(3), 0, 1);
-        field.add(btnList.get(4), 1, 1);
-        field.add(btnList.get(5), 2, 1);
-        field.add(btnList.get(6), 0, 2);
-        field.add(btnList.get(7), 1, 2);
-        field.add(btnList.get(8), 2, 2);
-        
-        btnAction(btnList, 0, textTurn);
-        btnAction(btnList, 1, textTurn);
-        btnAction(btnList, 2, textTurn);
-        btnAction(btnList, 3, textTurn);
-        btnAction(btnList, 4, textTurn);
-        btnAction(btnList, 5, textTurn);
-        btnAction(btnList, 6, textTurn);
-        btnAction(btnList, 7, textTurn);
-        btnAction(btnList, 8, textTurn);
-        
-        
+        //addig layouts to VBox
         completeLayout.getChildren().addAll(textTurn, field);
         
+        //adding layouts to the main layout
         layout.setCenter(completeLayout);
         
+        //button logic
+        for (int i = 0; i < 9; i++) {
+            buttonState.setOnClick(i, textTurn);
+        }
+        
+        //setting toDisplay
         Scene scene = new Scene(layout);
         
         stage.setScene(scene);
@@ -85,92 +75,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch("--title=TicTacToe");
     }
-    
-    public Button createButton() {
-        Button button = new Button();
-        button.setPrefSize(80, 80);
-        button.setFont(Font.font("Monospaced", 40));
-        return button;
-    }
-    
-    //creating arrayList and adding buttons to it
-    public ArrayList<Button> createButtonList() {
-        ArrayList<Button> btnList = new ArrayList();
-        for (int i = 0; i < 9; i++) {
-            btnList.add(createButton());
-        }
-        return btnList;
-    }
-    
-    //button logic
-    public void btnAction(ArrayList<Button> btnList, int position, Label label) {
-        
-        btnList.get(position).setOnAction(event -> {
-            if (label.getText().equals("Turn: X")) {
-                btnList.get(position).setText("X");
-                label.setText("Turn: O");
-                btnList.get(position).setDisable(true);
-                check(btnList,label,"X");
-                check(btnList,label,"O");
-
-            } else {
-                btnList.get(position).setText("O");
-                label.setText("Turn: X");
-                btnList.get(position).setDisable(true);
-                check(btnList,label,"X");
-                check(btnList,label,"O");
-
-            }
-        });
-        
-
-        
-    }
-    
-   public void check(ArrayList<Button> btnList, Label label, String value) {
-       
-       if (btnList.get(0).getText().equals(value) && btnList.get(1).getText().equals(value) && btnList.get(2).getText().equals(value)) {
-           label.setText("Winner is " + value);
-           disableAllButtons(btnList);
-       }
-       if (btnList.get(3).getText().equals(value) && btnList.get(4).getText().equals(value) && btnList.get(5).getText().equals(value)) {
-           label.setText("Winner is " + value);
-           disableAllButtons(btnList);
-       }
-       if (btnList.get(6).getText().equals(value) && btnList.get(7).getText().equals(value) && btnList.get(8).getText().equals(value)) {
-           label.setText("Winner is " + value);
-           disableAllButtons(btnList);
-       }
-       if (btnList.get(0).getText().equals(value) && btnList.get(3).getText().equals(value) && btnList.get(6).getText().equals(value)) {
-           label.setText("Winner is " + value);
-           disableAllButtons(btnList);
-       }
-       if (btnList.get(1).getText().equals(value) && btnList.get(4).getText().equals(value) && btnList.get(7).getText().equals(value)) {
-           label.setText("Winner is " + value);
-           disableAllButtons(btnList);
-       }
-       if (btnList.get(2).getText().equals(value) && btnList.get(5).getText().equals(value) && btnList.get(8).getText().equals(value)) {
-           label.setText("Winner is " + value);
-           disableAllButtons(btnList);
-       }
-       if (btnList.get(6).getText().equals(value) && btnList.get(4).getText().equals(value) && btnList.get(2).getText().equals(value)) {
-           label.setText("Winner is " + value);
-           disableAllButtons(btnList);
-       }
-       if (btnList.get(0).getText().equals(value) && btnList.get(4).getText().equals(value) && btnList.get(8).getText().equals(value)) {
-           label.setText("Winner is " + value);
-           disableAllButtons(btnList);
-       } else {
-           label.setText("Draw");
-       }
-       
-       
-   }
-   
-   public void disableAllButtons(ArrayList<Button> btnList) {
-       for (Button button : btnList) {
-           button.setDisable(true);
-       }
-   }
     
 }
---
+++ src/main/java/my/company/domain/ButtonState.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.company.domain;
+
+import java.util.ArrayList;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+
+/**
+ *
+ * @author JT
+ */
+public class ButtonState {
+    
+    private ArrayList<Button> btnList;
+    private String[][] state;
+    
+    public ButtonState() {
+        this.btnList = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            this.btnList.add(createButton());
+        }
+        this.state = new String[3][3];
+    }
+    
+    public void addToLayout(GridPane layout) {
+        int flag = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                layout.add(this.btnList.get(flag), j, i);
+                flag++;
+            }
+        }
+    }
+    
+    public void setOnClick(int position, Label label) {
+        this.btnList.get(position).setOnAction(event -> {
+            if (label.getText().equals("Turn: X")) {
+                this.btnList.get(position).setText("X");
+                this.btnList.get(position).setDisable(true);
+                label.setText("Turn: O");
+                positionOfIndex(position);
+                check("X", label);
+                check("O", label);
+                
+            } else {
+                this.btnList.get(position).setText("O");
+                label.setText("Turn: X");
+                this.btnList.get(position).setDisable(true);
+                positionOfIndex(position);
+                check("X", label);
+                check("O", label);
+            }
+            });
+        
+    }
+    
+    private void positionOfIndex(int position) {
+        if (position == 1) {
+            this.state[position - 1][position] = this.btnList.get(position).getText();
+        } else if (position == 2) {
+                    this.state[position - 2][position] = this.btnList.get(position).getText();
+                } else if (position == 3) {
+                    this.state[position - 2][position - 3] = this.btnList.get(position).getText();
+                } else if (position == 4) {
+                    this.state[position - 3][position - 3] = this.btnList.get(position).getText();
+                } else if (position == 5) {
+                    this.state[position - 4][position - 3] = this.btnList.get(position).getText();
+                } else if (position == 6) {
+                    this.state[position - 4][position - 6] = this.btnList.get(position).getText();
+                } else if (position == 7) {
+                    this.state[position - 5][position - 6] = this.btnList.get(position).getText();
+                } else if (position == 8) {
+                    this.state[position - 6][position - 6] = this.btnList.get(position).getText();
+                } else {
+                    this.state[position][position] = this.btnList.get(position).getText();
+                }
+    }
+    
+    private Button createButton() {
+        Button button = new Button();
+        button.setPrefSize(80, 80);
+        button.setFont(Font.font("Monospaced", 40));
+        return button;
+    }
+    
+    private void check(String value, Label label) {
+        int row1 = 0, row2 = 0, row3 = 0, column1 = 0, column2 = 0, column3 = 0, diagonalMain = 0, diagonalSec = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 0 && this.state[i][j].equals(value)) {
+                    row1++;
+                    
+                } else if (i == 1 && this.state[i][j].equals(value)) {
+                    row2++;
+                } else if (i == 2 && this.state[i][j].equals(value)) {
+                    row3++;
+                } else if (j == 0 && this.state[i][j].equals(value)) {
+                    column1++;
+                } else if (j == 1 && this.state[i][j].equals(value)) {
+                    column2++;
+                } else if (j == 2 && this.state[i][j].equals(value)) {
+                    column3++;
+                } else if (i == j && this.state[i][j].equals(value)) {
+                    diagonalMain++;
+                } else if (i + j == 2 && this.state[i][j].equals(value)) {
+                    diagonalSec++;
+                }
+            }
+        }
+        
+        if (row1 == 3) {
+            label.setText("Winner is " + value);
+            
+        }
+        
+    }
+    
+}
